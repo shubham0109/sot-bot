bca8219b784a416e3c05310ce49ab65f03f0feec
Shubham Kumar <shubham.kumar0109@gmail.com>
2018-03-20 18:07:13 +0530

    Adding the procfile

--- bot.js
+++ bot.js
@@ -2,11 +2,15 @@ let Twit = require('twit');
 const fs = require('fs');
 let config = require('./config');
 
-fs.readFile('data1.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    tweets = JSON.parse(data);
-    //console.log(tweets);  
-    let len = tweets.length;
-    let tweet = tweets[Math.floor(Math.random()*len)];  
-    console.log(tweet.text);
-});
+setInterval(callback, 2*1000);
+    function callback(){
+        fs.readFile('data1.json', 'utf8', function (err, data) {
+            if (err) throw err;
+            tweets = JSON.parse(data);
+            //console.log(tweets);  
+            let len = tweets.length;
+            let tweet = tweets[Math.floor(Math.random()*len)];  
+            console.log(tweet.text);
+        });
+    }
+    
