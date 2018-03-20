49c2cfd2bf89ee31cd0558403e33ea6896d47781
Shubham Kumar <shubham.kumar0109@gmail.com>
2018-03-20 19:12:20 +0530

    3RD

--- bot.js
+++ bot.js
@@ -2,15 +2,20 @@ let Twit = require('twit');
 const fs = require('fs');
 let config = require('./config');
 
-setInterval(callback, 2*1000);
+let T = new Twit(config);
+
+
+setInterval(callback, 20*1000);
     function callback(){
         fs.readFile('data1.json', 'utf8', function (err, data) {
             if (err) throw err;
             tweets = JSON.parse(data);
             //console.log(tweets);  
             let len = tweets.length;
-            let tweet = tweets[Math.floor(Math.random()*len)];  
-            console.log(tweet.text);
+            let tweet = tweets[Math.floor(Math.random()*len)]; 
+            T.post('statuses/update', { status: tweet.text }, function(err, data, response) {
+                console.log(data)
+            });
         });
     }
     
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+    consumer_key: 'GbnW5d6jC6eJT6Y7tErewNt6V',
+    consumer_secret: 'rxLXA2rt4DMTLqJQ7liAUZERVlQIdRBfpyLmEkfTZetApj3upe',
+    access_token: '975730282115584000-zfSevcBkEsdVqFTdibX8OWFKSiFWv2r',
+    access_token_secret: '5hEOmZYgm4sLl32NWAbvl87QvSlVRhUIIdlFy6HdY8vLa'
 }
