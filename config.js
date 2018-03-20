d7e2687d12f4d63d20806b76dad751837eedb0ec
shubham kumar <shubham.kumar0109@gmail.com>
2018-03-20 19:13:28 +0530

    delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'GbnW5d6jC6eJT6Y7tErewNt6V',
-    consumer_secret: 'rxLXA2rt4DMTLqJQ7liAUZERVlQIdRBfpyLmEkfTZetApj3upe',
-    access_token: '975730282115584000-zfSevcBkEsdVqFTdibX8OWFKSiFWv2r',
-    access_token_secret: '5hEOmZYgm4sLl32NWAbvl87QvSlVRhUIIdlFy6HdY8vLa'
-}
